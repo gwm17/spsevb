29e3cac67badacd318fb88ce8d5f6064405b6a84
Gordon McCann <gwm17@my.fsu.edu>
2023-02-12 17:10:36 -0500

    Fix default window size. Cleanup some junk in main

--- src/main.rs
+++ src/main.rs
@@ -3,8 +3,6 @@ mod ui;
 
 use crate::ui::app::EVBApp;
 
-//Temporary
-
 fn main() {
     simplelog::TermLogger::init(simplelog::LevelFilter::Info, 
                                 simplelog::Config::default(),
@@ -12,6 +10,6 @@ fn main() {
                                 simplelog::ColorChoice::Auto)
                             .unwrap();
     let mut native_options = eframe::NativeOptions::default();
-    native_options.initial_window_size = Some(eframe::epaint::Vec2 { x: 780.0, y: 420.0 });
+    native_options.initial_window_size = Some(eframe::epaint::Vec2 { x: 600.0, y: 430.0 });
     eframe::run_native("SPS Event Builder", native_options, Box::new(|cc| Box::new( EVBApp::new(cc) )));
 }
