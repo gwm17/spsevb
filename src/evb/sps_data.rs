edcf3a52953eb937ba52218e1187515149bf39d0
Gordon McCann <gwm17@my.fsu.edu>
2023-03-05 17:29:56 -0500

    Fix sps_data implementation, clean up dataframe creation. Thinking about how to make lists...

--- src/evb/sps_data.rs
+++ src/evb/sps_data.rs
@@ -2,11 +2,13 @@
 use super::compass_data::{CompassData, decompose_uuid_to_board_channel};
 use super::channel_map::{ChannelMap, SPSChannelType};
 
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 use std::hash::Hash;
 
 use strum::IntoEnumIterator;
 use strum_macros::{EnumIter, AsRefStr};
+
+use polars::prelude::*;
 
 const INVALID_VALUE: f64 = -1.0e6;
 
@@ -54,22 +56,43 @@ impl SPSDataField {
 
 #[derive(Debug, Clone)]
 pub struct SPSData {
-    pub fields: HashMap<SPSDataField, f64>
+    //Columns must always come in same order, so use sorted map
+    pub fields: BTreeMap<SPSDataField, Vec<f64>>,
+    rows: usize
 }
 
 impl Default for SPSData {
     fn default() -> Self {
         let fields = SPSDataField::get_field_vec();
-        let mut data = SPSData { fields: HashMap::new() };
-        fields.into_iter().for_each(|f| { data.fields.insert(f, INVALID_VALUE); });
+        let mut data = SPSData { fields: BTreeMap::new(), rows: 0 };
+        fields.into_iter().for_each(|f| { data.fields.insert(f, vec![]); });
         return data;
     }
 }
 
 impl SPSData {
 
-    pub fn new(event: Vec<CompassData>, map: &ChannelMap, weights: Option<(f64, f64)>) -> SPSData {
-        let mut data = SPSData::default();
+    //To keep columns all same length, push invalid values as necessary
+    fn push_defaults(&mut self) {
+        for field in self.fields.iter_mut() {
+            if field.1.len() < self.rows {
+                field.1.push(INVALID_VALUE)
+            }
+        }
+    }
+
+    //Update the last element to the given value
+    fn push_value(&mut self, field: &SPSDataField, value: f64) {
+        if let Some(list) = self.fields.get_mut(field) {
+            if let Some(back) = list.last_mut() {
+                *back = value;
+            }
+        }
+    }
+
+    pub fn append_event(&mut self, event: Vec<CompassData>, map: &ChannelMap, weights: Option<(f64, f64)>) {
+
+        self.rows += 1;
 
         let mut dfl_time = INVALID_VALUE;
         let mut dfr_time = INVALID_VALUE;
@@ -84,61 +107,61 @@ impl SPSData {
             };
             match channel_data.channel_type {
                 SPSChannelType::ScintLeft => {
-                    data.fields.insert(SPSDataField::ScintLeftEnergy, hit.energy);
-                    data.fields.insert(SPSDataField::ScintLeftShort, hit.energy_short);
-                    data.fields.insert(SPSDataField::ScintLeftTime, hit.timestamp);
+                    self.push_value(&SPSDataField::ScintLeftEnergy, hit.energy);
+                    self.push_value(&SPSDataField::ScintLeftShort, hit.energy_short);
+                    self.push_value(&SPSDataField::ScintLeftTime, hit.timestamp);
                 }
 
                 SPSChannelType::ScintRight => {
-                    data.fields.insert(SPSDataField::ScintRightEnergy, hit.energy);
-                    data.fields.insert(SPSDataField::ScintRightShort, hit.energy_short);
-                    data.fields.insert(SPSDataField::ScintRightTime, hit.timestamp);
+                    self.push_value(&SPSDataField::ScintRightEnergy, hit.energy);
+                    self.push_value(&SPSDataField::ScintRightShort, hit.energy_short);
+                    self.push_value(&SPSDataField::ScintRightTime, hit.timestamp);
                 }
 
                 SPSChannelType::Cathode => {
-                    data.fields.insert(SPSDataField::CathodeEnergy, hit.energy);
-                    data.fields.insert(SPSDataField::CathodeShort, hit.energy_short);
-                    data.fields.insert(SPSDataField::CathodeTime, hit.timestamp);
+                    self.push_value(&SPSDataField::CathodeEnergy, hit.energy);
+                    self.push_value(&SPSDataField::CathodeShort, hit.energy_short);
+                    self.push_value(&SPSDataField::CathodeTime, hit.timestamp);
                 }
 
                 SPSChannelType::DelayFrontRight => {
-                    data.fields.insert(SPSDataField::DelayFrontRightEnergy, hit.energy);
-                    data.fields.insert(SPSDataField::DelayFrontRightShort, hit.energy_short);
-                    data.fields.insert(SPSDataField::DelayFrontRightTime, hit.timestamp);
+                    self.push_value(&SPSDataField::DelayFrontRightEnergy, hit.energy);
+                    self.push_value(&SPSDataField::DelayFrontRightShort, hit.energy_short);
+                    self.push_value(&SPSDataField::DelayFrontRightTime, hit.timestamp);
                     dfr_time = hit.timestamp;
                 }
 
                 SPSChannelType::DelayFrontLeft => {
-                    data.fields.insert(SPSDataField::DelayFrontLeftEnergy, hit.energy);
-                    data.fields.insert(SPSDataField::DelayFrontLeftShort, hit.energy_short);
-                    data.fields.insert(SPSDataField::DelayFrontLeftTime, hit.timestamp);
+                    self.push_value(&SPSDataField::DelayFrontLeftEnergy, hit.energy);
+                    self.push_value(&SPSDataField::DelayFrontLeftShort, hit.energy_short);
+                    self.push_value(&SPSDataField::DelayFrontLeftTime, hit.timestamp);
                     dfl_time = hit.timestamp;
                 }
 
                 SPSChannelType::DelayBackRight => {
-                    data.fields.insert(SPSDataField::DelayBackRightEnergy, hit.energy);
-                    data.fields.insert(SPSDataField::DelayBackRightShort, hit.energy_short);
-                    data.fields.insert(SPSDataField::DelayBackRightTime, hit.timestamp);
+                    self.push_value(&SPSDataField::DelayBackRightEnergy, hit.energy);
+                    self.push_value(&SPSDataField::DelayBackRightShort, hit.energy_short);
+                    self.push_value(&SPSDataField::DelayBackRightTime, hit.timestamp);
                     dbr_time = hit.timestamp;
                 }
 
                 SPSChannelType::DelayBackLeft => {
-                    data.fields.insert(SPSDataField::DelayBackLeftEnergy, hit.energy);
-                    data.fields.insert(SPSDataField::DelayBackLeftShort, hit.energy_short);
-                    data.fields.insert(SPSDataField::DelayBackLeftTime, hit.timestamp);
+                    self.push_value(&SPSDataField::DelayBackLeftEnergy, hit.energy);
+                    self.push_value(&SPSDataField::DelayBackLeftShort, hit.energy_short);
+                    self.push_value(&SPSDataField::DelayBackLeftTime, hit.timestamp);
                     dbl_time = hit.timestamp;
                 }
 
                 SPSChannelType::AnodeFront => {
-                    data.fields.insert(SPSDataField::AnodeFrontEnergy, hit.energy);
-                    data.fields.insert(SPSDataField::AnodeFrontShort, hit.energy_short);
-                    data.fields.insert(SPSDataField::AnodeFrontTime, hit.timestamp);
+                    self.push_value(&SPSDataField::AnodeFrontEnergy, hit.energy);
+                    self.push_value(&SPSDataField::AnodeFrontShort, hit.energy_short);
+                    self.push_value(&SPSDataField::AnodeFrontTime, hit.timestamp);
                 }
 
                 SPSChannelType::AnodeBack => {
-                    data.fields.insert(SPSDataField::AnodeBackEnergy, hit.energy);
-                    data.fields.insert(SPSDataField::AnodeBackShort, hit.energy_short);
-                    data.fields.insert(SPSDataField::AnodeBackTime, hit.timestamp);
+                    self.push_value(&SPSDataField::AnodeBackEnergy, hit.energy);
+                    self.push_value(&SPSDataField::AnodeBackShort, hit.energy_short);
+                    self.push_value(&SPSDataField::AnodeBackTime, hit.timestamp);
                 }
 
                 _ =>  continue
@@ -150,45 +173,40 @@ impl SPSData {
         let mut x2 = INVALID_VALUE;
         if dfr_time != INVALID_VALUE && dfl_time != INVALID_VALUE {
             x1 = (dfl_time - dfr_time) * 0.5 * 1.0/2.1;
-            data.fields.insert(SPSDataField::X1, x1);
+            self.push_value(&SPSDataField::X1, x1);
         }
         if dbr_time != INVALID_VALUE && dbl_time != INVALID_VALUE {
             x2 = (dbl_time - dbr_time) * 0.5 * 1.0/1.98;
-            data.fields.insert(SPSDataField::X2, x2);
+            self.push_value(&SPSDataField::X2, x2);
         }
         if x1 != INVALID_VALUE && x2 != INVALID_VALUE {
             let diff = x1 -x2;
             if diff > 0.0 {
-                data.fields.insert(SPSDataField::Theta, (diff/36.0).atan());
+                self.push_value(&SPSDataField::Theta, (diff/36.0).atan());
             } else if diff < 0.0 {
-                data.fields.insert(SPSDataField::Theta, std::f64::consts::PI + (diff/36.0).atan());
+                self.push_value(&SPSDataField::Theta, std::f64::consts::PI + (diff/36.0).atan());
             } else {
-                data.fields.insert(SPSDataField::Theta, std::f64::consts::PI * 0.5);
+                self.push_value(&SPSDataField::Theta, std::f64::consts::PI * 0.5);
             }
 
             match weights {
-               Some(w) => data.fields.insert(SPSDataField::Xavg, w.0 * x1 + w.1 * x2),
-               None => data.fields.insert(SPSDataField::Xavg, INVALID_VALUE)
+               Some(w) => self.push_value(&SPSDataField::Xavg, w.0 * x1 + w.1 * x2),
+               None => self.push_value(&SPSDataField::Xavg, INVALID_VALUE)
             };
         }
 
-        return data;
+        self.push_defaults();
     }
 
-    pub fn is_default(&self) -> bool {
-        for f in &self.fields {
-            if *f.1 != INVALID_VALUE {
-                return false;
-            }
-        }
-        return true;
-    }
-    
-    pub fn fields(&self) -> &HashMap<SPSDataField, f64> {
-        &self.fields
+    pub fn convert_to_series(self) -> Vec<Series> {
+        self.fields.into_iter()
+                    .map(|field| -> Series {
+                        Series::new(field.0.as_ref(), field.1)
+                    })
+                    .collect()
     }
 
-    pub fn size() -> usize {
-        SPSDataField::get_field_vec().len() * (std::mem::size_of::<f64>() + std::mem::size_of::<SPSDataField>())
+    pub fn size(&self) -> usize {
+        SPSDataField::get_field_vec().len() * (std::mem::size_of::<f64>() + std::mem::size_of::<SPSDataField>()) * self.rows
     }
 }
